bf8ca5d9faf47316236262ecb3730e5125fce7bf
Hemanth Bijjala <mate8fl@gmail.com>
2018-07-16 15:38:36 -0700

    updated code

--- Hemanth RPI/distance.py
+++ Hemanth RPI/distance.py
@@ -20,30 +20,39 @@ signal.signal(signal.SIGINT, close)
 # set GPIO input and output channels
 GPIO.setup(pinTrigger, GPIO.OUT)
 GPIO.setup(pinEcho, GPIO.IN)
-
+distance = 0
+distance2 = 0
 while True:
-	# set Trigger to HIGH
-	GPIO.output(pinTrigger, True)
-	# set Trigger after 0.01ms to LOW
-	time.sleep(0.00001)
-	GPIO.output(pinTrigger, False)
+	for x in range (2):
+		# set Trigger to HIGH
+		GPIO.output(pinTrigger, True)
+		# set Trigger after 0.01ms to LOW
+		time.sleep(0.00001)
+		GPIO.output(pinTrigger, False)
 
-	startTime = time.time()
-	stopTime = time.time()
-
-	# save start time
-	while 0 == GPIO.input(pinEcho):
 		startTime = time.time()
-
-	# save time of arrival
-	while 1 == GPIO.input(pinEcho):
 		stopTime = time.time()
 
-	# time difference between start and arrival
-	TimeElapsed = stopTime - startTime
-	# multiply with the sonic speed (34300 cm/s)
-	# and divide by 2, because there and back
-	distance = (TimeElapsed * 34300) / 2
+		# save start time
+		while 0 == GPIO.input(pinEcho):
+			startTime = time.time()
 
-	print ("Distance: %.1f cm" % distance)
-	time.sleep(1)
+		# save time of arrival
+		while 1 == GPIO.input(pinEcho):
+			stopTime = time.time()
+
+		# time difference between start and arrival
+		TimeElapsed = stopTime - startTime
+		# multiply with the sonic speed (34300 cm/s)
+		# and divide by 2, because there and back
+		if x == 1:
+			distance = round((TimeElapsed * 34300) / 2, 0)
+		else:
+			distance2 = round((TimeElapsed *34300)/2,0)
+			print ("Distance1: "+str(distance)+ "Distance2: "+ str(distance2))
+		time.sleep(0.05)
+	if distance - distance2 > 0 and distance - distance2 < 10:
+		print ("object is moving futher away")
+	elif distance - distance2 < 0 and distance - distance2 > -10:
+		print("object is moving closer")
+
