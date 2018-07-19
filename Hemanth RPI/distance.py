410224ee136b48ede27b875760cf3fa726cf4b38
ByteC0d <30668603+ByteC0d@users.noreply.github.com>
2018-07-18 22:07:21 -0700

    Organizing things

--- Hemanth RPI/distance.py
+++ Hemanth RPI/distance.py
@@ -3,13 +3,7 @@ import time
 import signal
 import sys
 
-# use Raspberry Pi board pin numbers
-GPIO.setmode(GPIO.BCM)
-
-# set GPIO Pins
-pinTrigger = 18
-pinEcho = 24
-closer = 2
+#When you press Control-C.   Exits the program
 def close(signal, frame):
 	print("\nTurning off ultrasonic distance detection...\n")
 	GPIO.cleanup() 
@@ -17,46 +11,63 @@ def close(signal, frame):
 
 signal.signal(signal.SIGINT, close)
 
+def calcDistance():
+	# set Trigger to HIGH
+	GPIO.output(pinTrigger, True)
+	# set Trigger after 0.01ms to LOW
+	time.sleep(0.00001)
+	GPIO.output(pinTrigger, False)
+
+	startTime = time.time()
+	stopTime = time.time()
+
+	# save start time
+	while 0 == GPIO.input(pinEcho):
+		startTime = time.time()
+
+	# save time of arrival
+	while 1 == GPIO.input(pinEcho):
+		stopTime = time.time()
+
+	# time difference between start and arrival
+	TimeElapsed = stopTime - startTime
+	# multiply with the sonic speed (34300 cm/s)
+	# and divide by 2, because there and back
+	# Distance = Speed(34300 cm/s) * Time 
+	
+	distance = round((TimeElapsed * 34300) / 2, 0)
+	
+	return distance
+	
+
+# use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# set GPIO Pins
+pinTrigger = 18
+pinEcho = 24
+closer = 2
+
 # set GPIO input and output channels
 GPIO.setup(pinTrigger, GPIO.OUT)
 GPIO.setup(closer, GPIO.OUT)
 GPIO.setup(pinEcho, GPIO.IN)
 distance = 0
 distance2 = 0
-while True:
-	for x in range (2):
-		# set Trigger to HIGH
-		GPIO.output(pinTrigger, True)
-		# set Trigger after 0.01ms to LOW
-		time.sleep(0.00001)
-		GPIO.output(pinTrigger, False)
 
-		startTime = time.time()
-		stopTime = time.time()
-
-		# save start time
-		while 0 == GPIO.input(pinEcho):
-			startTime = time.time()
-
-		# save time of arrival
-		while 1 == GPIO.input(pinEcho):
-			stopTime = time.time()
-
-		# time difference between start and arrival
-		TimeElapsed = stopTime - startTime
-		# multiply with the sonic speed (34300 cm/s)
-		# and divide by 2, because there and back
-		if x == 1:
-			distance = round((TimeElapsed * 34300) / 2, 0)
-		else:
-			distance2 = round((TimeElapsed *34300)/2,0)
-			print ("Distance1: "+str(distance)+ "Distance2: "+ str(distance2))
-		time.sleep(0.25)
-
-	if distance - distance2 > 5:
+while True:	
+	distance = calcDistance()
+	time.sleep(0.05)
+	distance2 = calcDistance()
+	print ("Distance1: "+str(distance)+ "Distance2: "+ str(distance2))
+	time.sleep(0.25)
+	
+	if distance < 15 or distance2 < 15:
+		print("OBJECT IN CLOSE PROXIMITY")
+	if distance - distance2 > 1:
 		print ("object is moving further")
 		GPIO.output(closer, False)
-	elif distance - distance2 < -5:
+	elif distance - distance2 < -2:
 		print("object is moving closer")
 		GPIO.output(closer, True)
 
